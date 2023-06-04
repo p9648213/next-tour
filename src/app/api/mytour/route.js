5c65c74dcb0081c141f449264e3514e6560da1c9
p9648213 <p9648213750912@gmail.com>
2023-06-04 11:01:58 +0700

    update static route

--- src/app/api/mytour/route.js
+++ src/app/api/mytour/route.js
@@ -2,9 +2,15 @@ import { getMyBookingTours } from "@/utils/user-helper";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  const token = request.cookies.get("jwt").value;
+  const token = request.cookies.get("jwt");
 
-  const res = await getMyBookingTours(token);
-
-  return NextResponse.json(res);
+  if (token) {
+    const res = await getMyBookingTours(token.value);
+    return NextResponse.json(res);
+  } else {
+    return NextResponse.json({
+      status: "fail",
+      message: "You are not logged in",
+    });
+  }
 }
