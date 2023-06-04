40a5ab0adf1a7c07471ae29ada5fdbaf445eb530
p9648213 <p9648213750912@gmail.com>
2023-06-04 12:44:01 +0700

    update cache tour

--- src/utils/tour-helper.js
+++ src/utils/tour-helper.js
@@ -5,9 +5,7 @@ async function fetchAllTour(queryParams) {
       `${process.env.NEXT_PUBLIC_URL}/api/v1/tours/?sort=${sortString}${
         searchTerm && `&name=${searchTerm}`
       }&page=${page}&limit=${limit}`,
-      {
-        cache: "no-store",
-      }
+      { next: { revalidate: 86400 } }
     );
 
     return res.json();
@@ -20,24 +18,7 @@ async function fetchTour(params) {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_URL}/api/v1/tours/slug/${params}`,
-      {
-        cache: "no-store",
-      }
-    );
-
-    return res.json();
-  } catch (error) {
-    return { status: "fail", message: error.message };
-  }
-}
-
-async function fetchNumberOfTours() {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_URL}/api/v1/tours/numberoftours`,
-      {
-        cache: "no-store",
-      }
+      { next: { revalidate: 86400 } }
     );
 
     return res.json();
@@ -64,4 +45,4 @@ async function bookTour(tourId, token) {
   }
 }
 
-module.exports = { fetchAllTour, fetchTour, fetchNumberOfTours, bookTour };
+module.exports = { fetchAllTour, fetchTour, bookTour };
