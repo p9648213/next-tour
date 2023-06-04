8aaf5a74b348e8e064a1e75b93de555744079d14
p9648213 <p9648213750912@gmail.com>
2023-06-04 11:24:37 +0700

    update sharp

--- src/components/tour-overview/Pictures.js
+++ src/components/tour-overview/Pictures.js
@@ -4,19 +4,13 @@ export default function Pictures({ tour }) {
   let renderPicture = tour.images.map((img, index) => {
     return (
       <div key={index} className="picture-box">
-        {/* <Image
-          className={`picture-box__img picture-box__img--${index + 1}`}
-          src={`/img/tours/${img}`}
-          alt={`${tour.name} Tour ${index + 1}`}
-          loading="eager"
-          width={2000}
-          height={1333}
-        /> */}
-        <img
+        <Image
           className={`picture-box__img picture-box__img--${index + 1}`}
           src={`${process.env.NEXT_PUBLIC_URL}/img/tours/${img}`}
           alt={`${tour.name} Tour ${index + 1}`}
-        ></img>
+          width={2000}
+          height={1333}
+        />
       </div>
     );
   });
